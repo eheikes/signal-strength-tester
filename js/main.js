161c68941a71323358f813d846d00b65ea29199e
Eric Heikes <eheikes@gmail.com>
2014-02-18 12:08:59 -0600

    Add basic functionality.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+
   // Hide the no-JS alert.
   $('#no-js-msg').addClass('hidden');
+
+  // Set up the start/restart button.
+  $('button#start').click(function() {
+    tester.restart();
+    $(this).text('Clear Data & Restart');
+  });
+
+  // Update the screen periodically.
+  window.setInterval(function() {
+    var table = $('table#data');
+
+    if (!tester.isRunning) { return; }
+
+    $('.requests',   table).text(tester.numRequests);
+    $('.responses',  table).text(tester.numResponses);
+    $('.percentage', table).text(tester.percentSuccessful);
+
+    $('#strength').removeClass('hidden');
+
+  }, 500); // every half second
+
 });
---
+++ js/wifi-tester.js
@@ -0,0 +1,69 @@
+var WifiTester = (function() {
+  function WifiTester() {
+    this.httpType = 'HEAD';
+    this.testUrl  = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=test';
+    this.useJsonP = true;
+    this.timeout  = 1000; // request timeout, in millisecs
+    this.interval = 1000; // polling frequency, in millisecs
+
+    this.reset();
+  }
+
+  WifiTester.prototype.calc = function() {
+    this.percentSuccessful = this.numRequests / this.numResponses * 100;
+  };
+
+  WifiTester.prototype.reset = function() {
+    this.isRunning         = false;
+    this.numRequests       = 0;
+    this.numResponses      = 0;
+    this.percentSuccessful = 0;
+
+    if (this.intervalID) {
+      window.clearInterval(this.intervalID);
+      this.intervalID = null;
+    }
+  };
+
+  WifiTester.prototype.restart = function() {
+    this.reset();
+    this.start();
+  };
+
+  WifiTester.prototype.start = function() {
+    var tester = this;
+    tester.isRunning = true;
+    tester.intervalID = window.setInterval(function() {
+      tester.test();
+    }, tester.interval);
+  };
+
+  WifiTester.prototype.test = function() {
+    var tester = this;
+
+    tester.numRequests++;
+
+    var ajax = $.ajax({
+      url:      tester.testUrl,
+      type:     tester.httpType,
+      timeout:  tester.timeout,
+      dataType: tester.useJsonP ? 'jsonp' : 'text',
+      cache:    false
+    });
+
+    ajax.done(function(data, textStatus, jqXHR) {
+      tester.numResponses++;
+      tester.calc();
+    });
+
+    ajax.fail(function(jqXHR, textStatus, errorThrown) {
+      tester.calc();
+      console.log(textStatus);
+      console.log(errorThrown);
+    });
+  };
+
+  return WifiTester;
+})();
+
+var tester = new WifiTester();
